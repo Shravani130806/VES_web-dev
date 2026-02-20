87f9ebbc3feffe236dc21cd0efe089ffb44d2856
Shravani130806 <shravani.mankar10@gmail.com>
2026-02-20 22:01:59 +0530

    Exp 3 update

--- exp 3/index.js
+++ exp 3/index.js
@@ -2,3 +2,15 @@ function changeLanguage() {
   document.getElementById("portfolioText").innerText =
     "Je suis une étudiante en deuxième année d’ingénierie passionnée par le développement web, l’apprentissage de nouvelles technologies et la création de projets créatifs pour améliorer mes compétences.";
 }
+let count = 1;
+
+function addItem() {
+  const ul = document.getElementById("dynamicList");
+
+  const li = document.createElement("li");
+  li.textContent = "List item " + count;
+
+  ul.appendChild(li);
+
+  count++;
+}
