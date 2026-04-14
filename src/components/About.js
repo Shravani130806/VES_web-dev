b5362a80a62a59277ef578f6d4ee6fc5a87d48ae
Shravani130806 <shravani.mankar10@gmail.com>
2026-04-14 13:17:44 +0530

    Initial commit - baking website

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h2>About Us</h2>
+      <p>We bake fresh homemade desserts with love and care.</p>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section>
+      <h2>Freshly Baked Happiness</h2>
+      <button>Order Now</button>
+    </section>
+  );
+}
+
+export default Hero;
